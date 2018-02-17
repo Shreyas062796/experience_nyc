8d11a93dfb7980428dc62b6715ea5293d56a219d
Shreyas Ramanujam <Ramanujamshreyas4@gmail.com>
2018-02-16 22:06:00 -0500

    added more functions to the mongo connector

--- backend/mongoConnector.py
+++ backend/mongoConnector.py
@@ -2,34 +2,69 @@ from pymongo import *
 from places import *
 from bson.objectid import *
 
-def dbConnect():
+def placesConnect():
 	client = MongoClient('mongodb://localhost:27017/')
-	restaurantsdb = client.places
-	return(restaurantsdb)
+	placesdb = client.places
+	return(placesdb)
+
+def loginConnect():
+	client = MongoClient('mongodb://localhost:27017/')
+	logindb = client.loginInfo
+	return(logindb)
+
+def tagsConnect():
+	client = MongoClient('mongodb://localhost:27017/')
+	tagsdb = client.Tags
+	return(tagsdb)
 
 
 def populateRestaurants():
 	restaurants = getNYCRestaurants()
-	db = dbConnect()
+	db = placesConnect()
 	for restaurant in restaurants['results']:
 		db.restaurants.insert_one(restaurant)
 
 def populateBars():
 	bars = getNYCBars()
-	db = dbConnect()
+	db = placesConnect()
 	for bar in bars['results']:
 		db.bars.insert_one(bar)
 
 
-
-def getTest():
-	db = dbConnect()
+def getBars():
+	allBars = []
+	db = placesConnect()
 	for document in db.bars.find():
-		pprint(document)
+		allBars.append(document)
+	return(allBars)
 
+def populateLogin(login):
+	db = loginConnect()
+	db.Info.insert_one(tag)
 
+def authenticateLogin(login):
+	#authenticate login and return true or false
+
+def populateTags(tags):
+	db = tagsConnect()
+	for tag in tags:
+		db.Tags.insert_one(tag)
+
+def getRestaurants():
+	allRestaurants = []
+	db = placesConnect()
+	for document in db.restaurants.find():
+		allRestaurants.append(document)
+	return(allRestaurants)
+
+def QueryRestaurants(cost,rating):
+	for restaurant in getRestaurants():
+		if('price_level' in restaurant and 'rating' in restaurant):
+			if(restaurant['rating'] >= rating and restaurant['price_level'] >= cost):
+				pprint(restaurant)
 
 if __name__ == "__main__":
 	#populateRestaurants()
 	#populateBars()
-	getTest()
+	#print(getBars())
+	QueryRestaurants(3,4)
