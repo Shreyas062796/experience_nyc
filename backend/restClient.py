7a2677db82e8a5b2de8667fdbf43ffc670767bc9
Steven Dias <stevendias11@gmail.com>
2018-02-19 14:15:34 -0500

    flask is live and client updated to work on heroku

--- backend/restClient.py
+++ backend/restClient.py
@@ -1,39 +1,42 @@
 from flask import Flask, render_template, request, redirect, session
 import random, json
 from mongoConnector import *
+import sys
 
-restApi = Flask(__name__)
+# [print("{} {}".format(keys, values)) for keys,values in sys.modules(__name__).items()]
+
+restClient = Flask(__name__)
 
 #any personal or important info is a post request
 #any other information is get request
-@restApi.route('/createuser', methods = ['POST'])
+@restClient.route('/createuser', methods = ['POST'])
 def addUser():
 	info = requests.get_json()
-    populateLogin(info)
-    print("login data was populated")
-    #creates session when the person creates account
-    session['user'] = info['username']
+	populateLogin(info)
+	print("login data was populated")
+	#creates session when the person creates account
+	session['user'] = info['username']
 
 #authenticates user for database
-@restApi.route('/authenticate', methods = ['POST'])
+@restClient.route('/authenticate', methods = ['POST'])
 def auth():
-    info = requests.get_json()
-    if(authenticateLogin(info["username"],info["password"])):
-    	session['user'] = info["username"]
-    else:
-    	print("The password or the username that you have entered doesnt exist")
+	info = requests.get_json()
+	if(authenticateLogin(info["username"],info["password"])):
+		session['user'] = info["username"]
+	else:
+		print("The password or the username that you have entered doesnt exist")
 
 
-@restApi.route('/restaurants/', methods = ['POST'])#have some parameters
+@restClient.route('/restaurants/', methods = ['POST'])#have some parameters
 def getRestaurants():
-    #query db and return json to the front end
+	#query db and return json to the front end
+	pass
 
-@restApi.route('/restaurants/', methods = ['POST'])#have some parameters                 
-def getRestaurants():
-    #query db and return json to the front end 
-
+@restClient.route('/')
+def index():
+	return '<h1>It is live</h1>'
 
 
 if __name__ == '__main__':
-    app.run()
+	restClient.run()
 
