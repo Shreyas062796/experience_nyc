e4e140891f00e19f7e80239af9d7accdce999a79
ssd42 <stevendias11@gmail.com>
2018-02-24 18:45:29 -0500

    Added a cron feature to the rest client to update data as time passes, update functions not included

--- backend/restClient.py
+++ backend/restClient.py
@@ -1,14 +1,40 @@
 from flask import Flask, render_template, request, redirect, session
 import random, json
 from mongoConnector import *
-import sys, os
+import sys, os, time
 import json
-
 # [print("{} {}".format(keys, values)) for keys,values in sys.modules(__name__).items()]
 
 DEBUG = True
 
 restClient = Flask(__name__)
+
+# this works, it may not be the best way to do it, but works
+# this way whenever the server loads up you have data for the 
+# user to work with and will keep updating hourly
+@app.before_first_request
+def activate_job():
+	def get_data():
+		while True:
+			# updateEvents()	#write this later
+			# updatePlaces()	#write this later
+
+			time.sleep(3600) # sleep for an hour
+	#==============================================
+
+	# This is for the caching of data
+	# sets up the data for when the first first goes up
+	updateEvents()
+	updatePlaces()
+
+	hour = datetime.datetime.now().hour # for first setup
+	# keep checking when you reach the end of the first hour
+	while hour == datetime.datetime.now().hour:
+		time.sleep(60)
+
+	thread = threading.Thread(target=get_data)
+	thread.start()
+
 
 #any personal or important info is a post request
 #any other information is get request
@@ -44,34 +70,25 @@ def getEvents():
 	jsonfile = ''
 	if DEBUG:
 		jsonfile = cwd + '/example.json'
-
 	
 
 	jsonString = ''
 
 	with open(jsonfile, 'r') as File:
 		a = json.loads(File.read())
-		# print(a[0].keys())
 
 		returndic = list()
-
 		for i in range(len(a)):
 			temp = dict()
-			print(a[i]['rating'])
-
 			temp['rating'] = a[i]['rating']
 			temp['name'] = a[i]['name']
 			temp['opening_hours'] = a[i]['opening_hours']
 			# temp['price_level'] = a[i]['price_level']
-
 			returndic.append(temp)
 
 		jsonString = json.dumps(returndic)
 
 	return jsonString
-
-
-
 
 
 @restClient.route('/')
@@ -81,4 +98,3 @@ def index():
 
 if __name__ == '__main__':
 	restClient.run()
-
