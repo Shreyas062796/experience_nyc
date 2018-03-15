73f57c2c9e82d96bdf3a1d035af1ed5b3b152937
Steve Dias <stevendias11@gmail.com>
2018-03-15 11:55:02 -0400

    adding new files

--- backend/lib/sendmail.py
+++ backend/lib/sendmail.py
@@ -1,39 +1,56 @@
 import smtplib
 from email.mime.text import MIMEText
 import time, logging
+import time, hashlib
+
 
 # Logging
 LOG_NAME = 'Error_Log.log'
 logging.basicConfig(filename=LOG_NAME, level=logging.DEBUG)
+base_msg = "Hi there, thanks for creating an account at ExperienceNYC,\n\n\tin order to finalize your authentication please go to the following link.\n\t"
+base_url = "https://enyc-m.herokuapp.com/"
 
 
-class Sendmail:
+
+class sendMail:
 
 	def __init__(self, mymail, mypass):
 		self.mymail = mymail
 		self.mypass = mypass
 
 
-	def sendmail(self, email, subject, message):
-		
+	def Send(self, to_email, subject):
+		message, code = self._generateCode(to_email)
+		self._sendmail(to_email, subject, message)
+		# add_to_db(to_email, unique_code) # add the code to db to check when user clicks it
+
+
+	# ensure code is unique
+	def _generateCode(self, to_email):
+		h_email = hashlib.md5(to_email.encode('utf-8')).hexdigest()
+		unique_code = h_email+str(int(time.time()))
+		message = (base_msg+base_url+unique_code)
+		return message, unique_code
+
+	def _sendmail(self, to_email, subject, message):
 		text = MIMEText(message.encode('utf-8'), 'plain', 'utf-8')
 
 		text['From'] = self.mymail
-		text['To'] = email
+		text['To'] = to_email
 		text['Subject'] = subject
 
 		try:
-	        smtpObj = smtplib.SMTP('smtp.gmail.com:587') # ('localhost', 1025)
-	        smtpObj.ehlo()
-	        smtpObj.starttls()
-	        smtpObj.ehlo()
-	        smtpObj.login(self.mymail, self.mypass)
-	        smtpObj.sendmail(self.mymail, [email], text.as_string())
-	        smtpObj.quit()
-	        print("Successfully sent email")
-	    except Exception as e: # Something can go wrong from google's side or an update
-	        print("Error: unable to send email: {}".format(e))
-	        logging.debug(e)
+			smtpObj = smtplib.SMTP('smtp.gmail.com:587') # ('localhost', 1025)
+			smtpObj.ehlo()
+			smtpObj.starttls()
+			smtpObj.ehlo()
+			smtpObj.login(self.mymail, self.mypass)
+			smtpObj.sendmail(self.mymail, [to_email], text.as_string())
+			smtpObj.quit()
+			print("Successfully sent email")
+		except Exception as e: # Something can go wrong from google's side or an update
+			print("Error: unable to send email: {}".format(e))
+			logging.debug(e)
 
 
 
---
+++ backend/lib/test.py
@@ -0,0 +1,5 @@
+import sendmail
+
+a = sendmail.sendMail("experiencenycco@gmail.com", "anotherone_44")
+
+a.Send('ssd42@njit.edu', 'Auth')
