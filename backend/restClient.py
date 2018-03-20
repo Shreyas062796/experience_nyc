7b3cd96145806000200688debcd4490771f22fab
Shreyas Ramanujam <Ramanujamshreyas4@gmail.com>
2018-03-20 18:20:19 -0400

    changing a little

--- backend/restClient.py
+++ backend/restClient.py
@@ -60,9 +60,9 @@ def addUser():
 	info['verify'] = False
 	info['user_unique_id'] = mail.sendMail("experiencenycco@gmail.com","anotherone_44").generateCode(info['email'])
 	mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").populateLogin(info)
-	print("login data was populated")
+	return(True)
 	#creates session when the person creates account
-	return "<h1>User: {}</h1>".format(info['email'])
+	# return "<h1>User: {}</h1>".format(info['email'])
 
 #authenticates user for database
 @restClient.route('/authenticate', methods = ['POST'])
