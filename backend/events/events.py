314919f1a3998813c34ea967c0a1ea37a66ff41d
ssd42 <stevendias11@gmail.com>
2018-03-21 15:54:17 -0400

    adding filtering for events

--- backend/events/events.py
+++ backend/events/events.py
@@ -6,25 +6,19 @@ from datetime import datetime
 DEBUG = True
 # make this a library to get events for a person
 
-web_link = "https://www.eventbriteapi.com/v3/events/search/?location.within=6mi&location.latitude=40.785416&location.longitude=-73.963165&token="
-
+# web_link = "https://www.eventbriteapi.com/v3/events/search/?location.within=6mi&location.latitude=40.785416&location.longitude=-73.963165&token="
 
 # ========================
 # Globals
-
 BASE_LINK = "https://www.eventbriteapi.com/v3/events/search/?token=3PLRBVBROECU3VJOPGUY"
 WEB_LINK = "https://www.eventbriteapi.com/v3/events/search/?token=3PLRBVBROECU3VJOPGUY"
 
+VENUE_LINK = "https://www.eventbriteapi.com/v3/venues/:id/?token=3PLRBVBROECU3VJOPGUY"
 
 DEFAULT_KEYWORDS = ['music', 'dance', 'convention', 'exercise']
 # ========================
 
-
 class getEvents:
-
-
-
-
 	# force the location to decrese the amount of results
 	# have some of this data cached but allow the user to query
 	# some of it themselves
@@ -52,10 +46,28 @@ class getEvents:
 
 		# print(WEB_LINK)
 		data = requests.get(WEB_LINK)
-
 		events = data.json()['events']
 
 		return events
+
+	# gets address and information about a specific venue
+	def getVenueInfo(self, venue_id):
+		useful_data = dict()
+
+		venue = VENUE_LINK.replace(':id': str(venue_id))
+		data = requests.get(venue)
+
+		useful_data['address'] = data['address']['localized_address_display']
+		useful_data['latitude'] = data['latitude']
+		useful_data['longitude'] = data['longitude']
+		useful_data['id'] = data['id']
+		useful_data['name'] = data['name']
+
+		return useful_data
+
+
+
+
 
 
 def main():
