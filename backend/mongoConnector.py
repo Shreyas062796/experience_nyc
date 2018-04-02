3facb6baa105cf090b2cca15983fcc39a0bb173f
Addae Amofa-Boachie <aa944@njit.edu>
2018-04-01 20:56:00 -0400

    google map update

--- backend/mongoConnector.py
+++ backend/mongoConnector.py
@@ -1,81 +1,297 @@
 from pymongo import *
-from places import *
+import places as ps
 from bson.objectid import *
+import random
+import json
+import hashlib
+import uuid
+from pprint import pprint
 
+places = ps.NYCPlaces('AIzaSyA3wV-hPoa6m5Gxjcc_sZ2fyatNS21Pv0A',40.7831,-73.9712)
 
+class MongoConnector:
+	def __init__(self,clientHost,clientPort,username,password,database):
+		self.clientHost = clientHost
+		self.clientPort = clientPort
+		self.username = username
+		self.password = password
+		self.database = database
 
-def placesConnect():
-	client = MongoClient('mongodb://localhost:27017/')
-	placesdb = client.places
-	return(placesdb)
+	#connects to mongo server based on parameters
+	def clientConnect(self):
+		connection = 'mongodb://' + str(self.username) + ':' + str(self.password) + '@' + str(self.clientHost) + ':' + str(self.clientPort) + '/' + str(self.database)
+		client = MongoClient(connection).experience_nyc #places and users database
+		# client = MongoClient(connection).enyc #events database
+		return(client)
 
-def loginConnect():
-	client = MongoClient('mongodb://localhost:27017/')
-	logindb = client.loginInfo
-	return(logindb)
+	def EventsclientConnect(self):
+		connection = 'mongodb://' + str(self.username) + ':' + str(self.password) + '@' + str(self.clientHost) + ':' + str(self.clientPort) + '/' + str(self.database)
+		client = MongoClient(connection).enyc #events database
+		return(client)
+	#adds all places to places collection in database
+	def populatePlaces(self):
+		allplaces = places.getAllPlaces()
+		db = self.clientConnect()
+		count = 0
+		for placeId in allplaces:
+			for place in allplaces[placeId]['results']:
+				try:
+					db.places.insert_one(place)
+					print("populated")
+				except:
+					continue
 
-def tagsConnect():
-	client = MongoClient('mongodb://localhost:27017/')
-	tagsdb = client.Tags
-	return(tagsdb)
+	#gets all documents from places collection
+	def getPlaces(self):
+		allPlaces = []
+		db = self.clientConnect()
+		for document in db.places.find({}):
+			allPlaces.append(document)
+		return(allPlaces)
 
-#adds initital restaurant data to database
-def populateRestaurants():
-	restaurants = getNYCRestaurants()
-	db = placesConnect()
-	for restaurant in restaurants['results']:
-		db.restaurants.insert_one(restaurant)
+	#gets all the places in a certain radius around a coordinate
+	def getPlacesInRadius(self,lat,lng,radius):
+		allPlaces = []
+		db = self.clientConnect()
+		# db.places.find({'geometry.location':{'$geoWithin':{'$centerSphere': [[-73.93414657,40.82302903], 5]}}})
+		for document in db.places.find({'geometry.location':{'$nearSphere':[lng,lat],'$maxDistance':radius*1609}}):
+			allPlaces.append(document)
+		# pprint(allPlaces)
+		return(allPlaces)
 
-#adds initital Bars data to database
-def populateBars():
-	bars = getNYCBars()
-	db = placesConnect()
-	for bar in bars['results']:
-		db.bars.insert_one(bar)
+	#populates user collection with user information
+	def populateLogin(self,login):
+		db = self.clientConnect()
+		login['password'] = hashlib.md5(login['password'].encode('utf-8')).hexdigest()
+		db.users.insert_one(login)
 
-#test to see if you can get all the bar data in database
-def getBars():
-	allBars = []
-	db = placesConnect()
-	for document in db.bars.find():
-		allBars.append(document)
-	return(allBars)
+	#authenticating login given a username and
+	def authenticateLogin(self,username,password):
+		db = self.clientConnect()
+		login = db.users.find_one({"username": username})
+		if(login):
+			if(login["password"] == hashlib.md5(password.encode('utf-8')).hexdigest()):
+				return(True)
+		return(False)
 
-#populates login table with json data
-def populateLogin(login):
-	db = loginConnect()
-	db.Info.insert_one(tag)
-
-def authenticateLogin(username,password):
-	db = loginConnect()
-	login = db.Info.find_one({"username": username})
-	if(login):
-		if(login["password"] == password):
+	#verifies email with the user 
+	def verifyEmail(self,username,unique_id):
+		db = self.clientConnect()
+		user = db.users.find_one({"username": username})
+		if(user['user_unique_id'] == unique_id):
+			user['verify'] = True
 			return(True)
-	return(False)
-	#authenticate login and return true or false
+		return(False)
 
+	#populates the tags
+	def addTags(self,username,tags):
+		db = self.clientConnect()
+		user = db.users.find_one({"username": username})
+		for tag in tags:
+			if(tag not in user['tags']):
+				db.users.update_one({'username': username},{'$push':{'tags':tag}})
+				return("Added")
+			else:
+				return("Already Exists")
 
-def populateTags(tags):
-	db = tagsConnect()
-	for tag in tags:
-		db.Tags.insert_one(tag)
+	def removeTags(self,username,tags):
+		db = self.clientConnect()
+		for tag in tags:
+			db.users.update_one({'username': username},{'$pull':{'tags':tag}})
+			print("removed")
 
-def getRestaurants():
-	allRestaurants = []
-	db = placesConnect()
-	for document in db.restaurants.find():
-		allRestaurants.append(document)
-	return(allRestaurants)
+	def getTags(self,username):
+		db = self.clientConnect()
+		user = db.users.find_one({"username": username})
+		return(user['tags'])
 
-def QueryRestaurants(cost,rating):
-	for restaurant in getRestaurants():
-		if('price_level' in restaurant and 'rating' in restaurant):
-			if(restaurant['rating'] >= rating and restaurant['price_level'] >= cost):
-				pprint(restaurant)
+		#authenticate login and return true or false
+	#adds favorite places to user profile
+	def addFavoritePlaces(self,username,place_id):
+		db = self.clientConnect()
+		user = db.users.find({'username': username})
+		if(place_id not in db.users.find({'username': username})):
+			db.users.update_one({'username': username},{'$push':{'favorite_places':place_id}})
+			return("Added")
+		else:
+			return("Already Exists")
+
+	#removes favorite places to user profile
+	def removeFavoritePlaces(self,username,place_id):
+		db = self.clientConnect()
+		db.users.update_one({'username': username},{'$pull':{'favorite_places':place_id}})
+		print("added")
+
+	#gets all the favorite place id's from the user profile
+	def getFavoritePlacesIds(self,username):
+		db = self.clientConnect()
+		user = db.users.find_one({"username": username})
+		print(user['favorite_places'])
+		return(user['favorite_places'])
+
+	#gets all the favorite places from the user profile
+	def getFavoritePlaces(self,username):
+		db = self.clientConnect()
+		favoritePlaces = []
+		user = db.users.find_one({"username": username})
+		for placeId in user['favorite_places']:
+			place = db.places.find_one({"id": placeId})
+			if(place is not None):
+				place['_id'] = str(place['_id'])
+				favoritePlaces.append(place)
+		return(favoritePlaces)
+
+	#adds a saved trip place to the user profile 
+	def addTripPlaces(self,username,place_id):
+		db = self.clientConnect()
+		user = db.users.find({'username': username})
+		if(place_id not in db.users.find({'username': username})):
+			db.users.update_one({'username': username},{'$push':{'current_trip_places':place_id}})
+			return("Added")
+		else:
+			return("Already Exists")
+
+	#removes a saved trip place to the user profile
+	def removeTripPlaces(self,username,place_id):
+		db = self.clientConnect()
+		db.users.update_one({'username': username},{'$pull':{'current_trip_places':place_id}})
+		print("added")
+
+	#gets all the saved trip placeid's to the user profile
+	def getTripPlacesIds(self,username):
+		db = self.clientConnect()
+		user = db.users.find_one({"username": username})
+		return(user['current_trip_places'])
+
+	#gets all the saved trip places by the user
+	def getTripPlaces(self,username):
+		db = self.clientConnect()
+		TripPlaces = []
+		user = db.users.find_one({"username": username})
+		for placeId in user['current_trip_places']:
+			place = db.places.find_one({"id": placeId})
+			if(place is not None):
+				place['_id'] = str(place['_id'])
+				TripPlaces.append(place)
+		return(TripPlaces)
+
+	#gets the user info for the user profile
+	def getUserInfo(self, username):
+		db = self.clientConnect()
+		user = db.users.find_one({"username": username})
+		print(user)
+		user['_id'] = str(user['_id'])
+		return(user)
+	
+	#dynamically querying places
+	def queryPlaces(self,types,price,num):
+		db = self.clientConnect()
+		params = {}
+		queriedPlaces = []
+		if types == ['']:
+			types = ['restaurant','cafe','bar','florist','amusement_park','bakery','clothing_store','convenience_store','department_store','hair_care','library','movie_theater','museum','night_club'
+			,'stadium','store','zoo']
+		if price == ['']:
+			price = [1,2,3]
+		else:
+			for i in range(len(price)):
+				price[i] = len(price[i])
+		for place in db.places.find({'$and':[{'types':{'$in': types},'price_level':{'$in':price}}]}):
+			place['_id'] = str(place['_id'])
+			if('photos' in place):
+				queriedPlaces.append(place)
+		if(len(queriedPlaces) == 0):
+			return([])
+		random.shuffle(queriedPlaces)
+		x = queriedPlaces[:num]
+		return(x)
+
+	def getQueriedPlaces(self,placeIds):
+		places = []
+		db = self.clientConnect()
+		for placeid in placeIds:
+			place = db.places.find_one({'id':placeid})
+			place['_id'] = str(place['_id'])
+			places.append(place)
+		return(places)
+# 	{
+#   trip_id:"1242112",
+#   user:"bored kid on speed",
+#   num_places:3,
+#   public: true/false,
+#   rating: 3,
+#   distance: 2.3,
+#   places: [
+#     Have json about places, in order of places you visited
+
+#   ],
+#   add some more stuff whatever you feel is important
+# }
+	#creates a trip
+	def createTrip(self,placeIds,trip_id,user,distance):
+		tripPlaces = []
+		trip = {}
+		db = self.clientConnect()
+		places = self.getPlaces()
+		for place_id in placeIds:
+			place = db.places.find_one({"id": place_id})
+			place['user_rating'] = None
+			tripPlaces.append()
+		trip['trip_id'] = trip_id
+		trip['user'] = user
+		trip['number_of_places'] = len(tripPlaces)
+		trip['public'] = False
+		trip['rating'] = None
+		trip['distance'] = distance
+		trip['places'] = tripPlaces
+		return(trip)
+
+	#adds to the database
+	def populateTrip(self,trip):
+		db = self.clientConnect()
+		db.trips.insert_one(trip)
+		return("added")
+
+	#query the trips by the username
+	def queryTrip(self,username,rating=None):
+		trips = []
+		db = self.clientConnect()
+		for trip in db.trips.find({'user': username}):
+			trips.append(trip)
+		return(trips)
+
+	#code to update trips and make them public
+	def makeTripPublic(self,tripId):
+		db = self.clientConnect()
+		db.trips.update_one({'trip_id': tripId},{'public':True})
+		return("updated")
+
+	#updates the trip rating
+	def updateTripRating(self,tripId,rating):
+		db = self.clientConnect()
+		db.trips.update_one({'trip_id': tripId},{'rating':rating})
+		return("updated")
+	#updates the place rating
+	def updatePlaceRating(self,tripId,placeId,rating):
+		db = self.clientConnect()
+
 
 if __name__ == "__main__":
-	#populateRestaurants()
-	#populateBars()
-	#print(getBars())
-	QueryRestaurants(3,4)
+	Experience = MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc")
+	# Experience.getUserInfo('test1')
+	# Experience = MongoConnector('ds123619.mlab.com', '23619', 'admin', 'admin', 'enyc'
+	# Experience.populatePlaces()
+	# pprint(Experience.getPlacesInRadius(40.7733125,-73.9837555,2))
+	# print(Experience.getFavoritePlaces('test1'))
+	# Experience.getBars()
+	# Experience.getRestaurants()
+	# pprint(Experience.QueryRestaurants(2,2,2))
+	print(Experience.queryPlaces([''],[''],100))
+	# pprint(Experience.QueryBars(2,2,2))
+	# Experience.addFavoritePlaces("testUser",134)
+	# tripnames = ['dastrip','drunknight','badnight','boys are lit','drama is bad']
+	# for i in tripnames:
+	# 	trip = Experience.createTrip(3,i)
+	# 	Experience.populateTrip(trip)
+	# pprint(Experience.queryTrip('goat'))
+	#QueryRestaurants(3,4)
--- backend/restClient.py
+++ backend/restClient.py
@@ -1,81 +1,277 @@
+# python libraries
 from flask import Flask, render_template, request, redirect, session, jsonify
+from flask_cors import CORS
 import random, json
-from mongoConnector import *
-import sys, os, time
+import sys, os, time, threading, requests
 import json
-import filtering
+import datetime
 
-# [print("{} {}".format(keys, values)) for keys,values in sys.modules(__name__).items()]
+# our libraries
+import mongoConnector as mg
+import reccomendations as rec
+import filtering
+import lib.sendmail as mail
+from events import events, events_script
+from maps.geo import addressToGeo
+from lib.caching import Cacher, EventCacher
+
 
 DEBUG = True
+CACHE = Cacher()
+EVENT_CACHE = EventCacher()
+
+
 
 restClient = Flask(__name__)
+CORS(restClient)
 
 # this works, it may not be the best way to do it, but works
 # this way whenever the server loads up you have data for the 
 # user to work with and will keep updating hourly
+
+def isNotNull(astr,request):
+	try:
+		return request.args[astr]
+	except KeyError:
+		return None
+
 @restClient.before_first_request
 def activate_job():
 	def get_data():
+		# events_script.initialPopulate()
 		hour = datetime.datetime.now().hour # for first setup
 		# keep checking when you reach the end of the first hour
 		while hour == datetime.datetime.now().hour:
 			time.sleep(60)
 		while True:
-			# updateEvents()	#write this later
-			# updatePlaces()	#write this later
 
-			time.sleep(3600) # sleep for an hour
-	#==============================================
+			time.sleep(810) # sleep for an hour
+			requests.get('https://experiencenyc.herokuapp.com/') # ping so it doesn't fall sleep
+			time.sleep(90)
+			print('Hour Notification')
+			EVENT_CACHE.setTopToday(events_script.getEvents().getEventsOfTheDay())
 
-	# This is for the caching of data
-	# sets up the data for when the first first goes up
-	# updateEvents()
-	# updatePlaces()
-
-	
-
+	EVENT_CACHE.setTopToday(events_script.getEvents().getEventsOfTheDay())
 	thread = threading.Thread(target=get_data)
 	thread.start()
 
-
+@restClient.route('/getuserdata', methods=['GET'])
+def getUserData():
+	username = request.get_json()['username']
+	# print(info)
+	# info = request.args['username']
+	userinfo = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getUserInfo(username)
+	return jsonify(userinfo)
 #any personal or important info is a post request
 #any other information is get request
+#{"firstName":"Alex","lastName":"Markenzon","username":"testUsername","password":"","email":"testemail@gmial.com"}:
 @restClient.route('/createuser', methods = ['POST'])
 def addUser():
-	info = requests.get_json()
-	populateLogin(info)
-	print("login data was populated")
-	#creates session when the person creates account
-	session['user'] = info['username']
+	print(request.is_json)
+	info = request.get_json() 
+	print(info)
+	info['verify'] = False
+	info['user_unique_id'] = mail.sendMail("experiencenycco@gmail.com","anotherone_44").generateCode(info['email'])
+	info['favorite_places'] = []
+	info['current_trip_places'] = []
+	info['tags'] = []
+	mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").populateLogin(info)
+	return(jsonify({"response":"True"}))
 
 #authenticates user for database
 @restClient.route('/authenticate', methods = ['POST'])
 def auth():
-	info = requests.get_json()
-	if(authenticateLogin(info["username"],info["password"])):
-		session['user'] = info["username"]
+	info = request.get_json()
+	if(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").authenticateLogin(info["username"],info["password"])):
+		return(jsonify({"response":"True"}))
 	else:
-		print("The password or the username that you have entered doesnt exist")
+		return(jsonify({"response":"False"}))
 
 
-@restClient.route('/restaurants/', methods = ['POST'])#have some parameters
-def getRestaurants():
-	#query db and return json to the front end
-	pass
+@restClient.route('/verify', methods = ['POST'])
+def verify():
+	info = request.get_json()
+	if(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").verifyEmail(info['username'],info['unique_id']) == True):
+		return(jsonify({"response":"The email was verified"}))
+	else:
+		return(jsonify({"response":"The email was not verified try again"}))
+	#username,unique_id,email
+
+# I'll need a function from you (addToFavorites) tha will take a unique place id as a single param and inserts it into the db as a list of favorite places
+# I'll also need you to write a function that will retreive the favorites returned as a json list
+@restClient.route('/addfavoriteplaces', methods=['POST'])
+def addfavoriteplaces():
+	info = request.get_json()
+	favorite = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").addFavoritePlaces(info['username'],info['place_id'])
+	if(favorite == "Added"):
+		return(jsonify({"response":"True"}))
+	else:
+		return(jsonify({"response":"Place already Exists"}))
+
+@restClient.route('/removefavoriteplaces', methods=['POST'])
+def removefavoriteplaces():
+	info = request.get_json()
+	mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").removeFavoritePlaces(info['username'],info['place_id'])
+	return(jsonify({"response":"True"}))
+
+@restClient.route('/getfavoriteplacesIds', methods=['POST'])
+def getfavoriteplacesIds():
+	info = request.get_json()
+	return(jsonify(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getFavoritePlacesIds(info['username'])))
+
+@restClient.route('/getfavoriteplaces', methods=['POST'])
+def getfavoriteplaces():
+	info = request.get_json()
+	return(jsonify(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getFavoritePlaces(info['username'])))
+
+@restClient.route('/addtripplaces', methods=['POST'])
+def addtripplaces():
+	info = request.get_json()
+	favorite = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").addTripPlaces(info['username'],info['place_id'])
+	if(favorite == "Added"):
+		return(jsonify({"response":"True"}))
+	else:
+		return(jsonify({"response":"Place already Exists"}))
+
+@restClient.route('/removetripplaces', methods=['POST'])
+def removetripplaces():
+	info = request.get_json()
+	mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").removeTripPlaces(info['username'],info['place_id'])
+	return(jsonify({"response":"True"}))
+
+@restClient.route('/gettripplacesIds', methods=['POST'])
+def gettripplacesIds():
+	info = request.get_json()
+	return(jsonify(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getTripPlacesIds(info['username'])))
+
+@restClient.route('/gettripplaces', methods=['POST'])
+def gettripplaces():
+	info = request.get_json()
+	return(jsonify(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getTripPlaces(info['username'])))
+
+@restClient.route('/addtags', methods=['POST'])
+def addTags():
+	info = request.get_json()
+	mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").addTags(info['username'],info['tags'])
+	return(jsonify({"response":"True"}))
+
+@restClient.route('/removetags', methods=['POST'])
+def removeTags():
+	info = request.get_json()
+	return(jsonify(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").removeTags(info['username'],info['tags'])))
+
+@restClient.route('/gettags', methods=['POST'])
+def getTags():
+	info = request.get_json()
+	return(jsonify(mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getTags(info['username'])))
+
+@restClient.route('/queryplaces', methods=['GET'])
+def queryplaces():
+	if request.method == 'GET':
+		num = request.args['num']
+		price_level = request.args.getlist('price_level[]')
+		types = request.args.getlist('types[]')
+		places = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").queryPlaces(types,price_level,int(num))
+		if(places):
+			return(jsonify(places))
+		elif(places == []):
+			return(jsonify({"response":"There is no values"}))
+
+@restClient.route('/getqueryplaces', methods=['GET'])
+def getqueryplaces():
+	if request.method == 'GET':
+		placeIds = request.args.getlist('placeIds[]')
+		print(placeIds)
+		places = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").getQueriedPlaces(placeIds)
+		if(places):
+			return(jsonify(places))
+		elif(places == []):
+			return(jsonify({"response":"There is no values"}))
+	# info = request.get_json()
+	# print(info['price_level'])
+	# print(info['types'])
+	# print(info['num'])
+	# places = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").queryPlaces(info['types'],info['price_level'],int(info['num']))
+	# if(places):
+	# 	return(jsonify(places))
+	# else:
+	# 	return(jsonify({"response":"There is no values"}))
+
+@restClient.route('/todayevents', methods=['GET'])
+def getTopEvents():
+	if request.method == "GET":
+		n = int(request.args['amount'])
+		return jsonify(EVENT_CACHE.getTopN(n))
+
+
 
 # gets bars that right now have preset coordinates
 @restClient.route('/topbars/<amount>', methods = ['GET'])#have some parameters
 def getTopBars(amount):
 	defaultlat  = 40.7831
 	defaultlong = 73.9712
-
 	myobj = filtering.Filtering(defaultlat, defaultlong)
+	print(type(jsonify(myobj.getTopBars(int(amount)))))
 	return jsonify(myobj.getTopBars(int(amount)))
 
+#temporary for testing geochange
+# and everything will be passed as a querystrin
+# this works for new places as your trip grows
+@restClient.route('/reccomendplaces', methods=['POST'])
+def getReccomendations():
+	info = request.get_json()
+	reccomendations = rec.placeReccomendations(info['username'],info['address']).PlaceReccomendation()
+	return(reccomendations)
 
-@restClient.route('/events', methods = ['POST', 'GET'])
+@restClient.route('/createTrip', methods=['POST','GET'])
+def createTrip():
+	#for if the user is not logged in
+	# placeIds,trip_id,user,distance
+	placeIds = request.args['placeIds']
+	trip_id = request.args['trip_id']
+	distance = request.args['types']
+	if(request.method == 'POST'):
+		username = request.args['username']
+	trip = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").createTrip()
+	populated = mg.MongoConnector("ds163918.mlab.com","63918","admin","admin","experience_nyc").populateTrip(trip)
+	if(populated == "added"):
+		return(jsonify({"response":"Trip Added"}))
+
+@restClient.route('/queryTrips', methods=['POST','GET'])
+def queryTrip():
+	pass
+
+@restClient.route('/getevents_temp')
 def getEvents():
+	# Checks if key exists, work this out later
+	def getkey(a_str, a_request):
+		try:
+			return a_request.args[a_str]
+		except KeyError:
+			return None
+	
+	#default lat long for now
+	lat = 40.7831
+	lon = -73.9712
+
+	gevents = events.getEvents()
+
+	if request.method== 'GET':
+		q = getkey('q', request)
+		address = getkey('address', request)
+
+		place = addressToGeo(address)   
+		lat, lng = place['lat'], place['lng']
+
+
+		eb_events = gevents.setParams(lat, lng, 2)      
+		# print(type(eb_events))
+		return jsonify(eb_events)
+
+	return 'Not calling the proper method'
+
+@restClient.route('/events_old', methods = ['GET'])
+def getEvents_old():
 	#temporary just for front testing
 
 	# this block is for heroku
@@ -101,13 +297,12 @@ def getEvents():
 
 		jsonString = json.dumps(returndic)
 
-	return jsonString
-
+	return(jsonString)
 
 @restClient.route('/')
 def index():
-	return '<h1>It is live</h1>'
-
+	return '<h1>Flask Client is up and running</h1>'
 
 if __name__ == '__main__':
-	restClient.run()
+	restClient.run(debug=DEBUG)
+
